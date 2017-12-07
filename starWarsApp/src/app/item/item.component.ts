069e9050d5b9e5ff2ded1d631cdd4428476c8d1e
saibot777 <stefantrajkovic777@gmail.com>
2017-12-07 12:28:30 +0100

    inputing characters to child components

--- starWarsApp/src/app/item/item.component.ts
+++ starWarsApp/src/app/item/item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-item',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item.component.css']
 })
 export class ItemComponent implements OnInit {
+  @Input() character;
 
   constructor() { }
 
--- starWarsApp/src/app/tabs/tabs.component.ts
+++ starWarsApp/src/app/tabs/tabs.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tabs.component.css']
 })
 export class TabsComponent implements OnInit {
+  characters = [
+    { name: 'Luke Skywalker', side: '' },
+    { name: 'Darth Vader', side: '' }
+  ];
+  chosenList = 'all';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onChoose(side) {
+    this.chosenList = side;
+  }
+
+  getCharacters() {
+    if(this.chosenList === 'all') {
+      return this.characters.slice();
+    }
+    return this.characters.filter((char) => {
+      return char.side === this.chosenList;
+    });
   }
 
 }
