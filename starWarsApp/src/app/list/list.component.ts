ccf40e43e77c4169cbfb46af074230f550bdedec
saibot777 <stefantrajkovic777@gmail.com>
2017-12-08 15:21:48 +0100

    add unsubscription to prevent memory leaks

--- starWarsApp/src/app/list/list.component.ts
+++ starWarsApp/src/app/list/list.component.ts
@@ -1,30 +1,36 @@
-import {Component, Input, OnInit } from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {StarWarsService} from '../star-wars.service';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent implements OnInit {
+export class ListComponent implements OnInit, OnDestroy{
   characters = [];
   loadedSide = 'all';
+  public subscription: Subscription;
 
   constructor(public route: ActivatedRoute, private swService: StarWarsService) { }
 
   ngOnInit() {
     this.route.params.subscribe(
       (params) => {
-        this.characters = this.swService.getCharacters(params.side)
-        this.loadedSide = params.side
+        this.characters = this.swService.getCharacters(params.side);
+        this.loadedSide = params.side;
       }
     );
-    this.swService.charactersChanged.subscribe(
+    this.subscription = this.swService.charactersChanged.subscribe(
       () => {
         this.characters = this.swService.getCharacters(this.loadedSide);
       }
     )
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
