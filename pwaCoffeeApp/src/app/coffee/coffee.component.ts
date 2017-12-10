27563a7ccfb82b50cad1d45ebbe07fdf39b465ae
saibot777 <stefantrajkovic777@gmail.com>
2017-12-10 09:44:04 +0100

    added ratings

--- pwaCoffeeApp/src/app/coffee/coffee.component.ts
+++ pwaCoffeeApp/src/app/coffee/coffee.component.ts
@@ -2,6 +2,7 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription} from 'rxjs/Subscription';
 import {Coffee} from '../core/models/coffee.model';
+import {GeolocationService} from '../core/services/geolocation.service';
 
 @Component({
   selector: 'app-coffee',
@@ -18,7 +19,7 @@ export class CoffeeComponent implements OnInit, OnDestroy {
     "Frappe"
   ];
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private geoService: GeolocationService) { }
 
   ngOnInit() {
     this.coffee = new Coffee();
@@ -26,6 +27,13 @@ export class CoffeeComponent implements OnInit, OnDestroy {
       this.route.params.subscribe(params => {
         console.log(params["id"]);
       });
+
+    this.geoService.requestLocation(location => {
+      if (location) {
+        this.coffee.location.latitude = location.latitude;
+        this.coffee.location.longitude = location.longitude;
+      }
+    });
   }
 
   ngOnDestroy() {
--- pwaCoffeeApp/src/app/core/models/coffee.model.ts
+++ pwaCoffeeApp/src/app/core/models/coffee.model.ts
@@ -8,6 +8,9 @@ export class Coffee {
   notes: string;
   tastingRating: TastingRating;
 
-  constructor(public name: string = "", public place: string = "", public location: PlaceLocation = null) {}
+  constructor(public name: string = "", public place: string = "", public location: PlaceLocation = null) {
+    this.location = new PlaceLocation();
+    this.tastingRating = new TastingRating();
+  }
 
 }
