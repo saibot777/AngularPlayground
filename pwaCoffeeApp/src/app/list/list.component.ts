cdedc1dab888cceb8a041132732421a2cdbe56c1
saibot777 <stefantrajkovic777@gmail.com>
2017-12-11 16:20:36 +0100

    share and map funcs added

--- pwaCoffeeApp/src/app/list/list.component.ts
+++ pwaCoffeeApp/src/app/list/list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DataService } from '../core/services/data.service';
 import {Coffee} from '../core/models/coffee.model';
 import {Router} from '@angular/router';
+import {GeolocationService} from '../core/services/geolocation.service';
 
 @Component({
   selector: 'app-list',
@@ -11,7 +12,10 @@ import {Router} from '@angular/router';
 export class ListComponent implements OnInit {
   list: [Coffee];
 
-  constructor(private dataService: DataService, private router: Router) { }
+  constructor(
+    private dataService: DataService,
+    private geoService: GeolocationService,
+    private router: Router) { }
 
   ngOnInit() {
     this.dataService.getList(list => {
@@ -21,6 +25,26 @@ export class ListComponent implements OnInit {
 
   goToDetails(coffee: Coffee) {
     this.router.navigate(["/coffee", coffee._id]);
+  }
+
+  goToMap(coffee: Coffee) {
+    const mapUrl = this.geoService.getMapLink(coffee.location);
+    location.href = mapUrl;
+  }
+
+  share(coffee: Coffee) {
+    const shareText = `I Had this coffee at ${coffee.place} and for me it's a ${coffee.rating}`;
+    if ('share' in navigator) {
+      navigator["share"]({
+        title: coffee.name,
+        text: shareText,
+        url: window.location.href
+      }).then(() => console.log("shared"))
+        .catch(() => console.log("error"));
+    } else {
+      const shareURL = `whatsapp://send?text=${encodeURIComponent(shareText)}`;
+      location.href = shareURL;
+    }
   }
 
 }
