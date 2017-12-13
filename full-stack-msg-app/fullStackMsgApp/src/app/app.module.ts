1e120efc0b6b2d380855ef37717f5f51c3ba52ae
saibot777 <stefantrajkovic777@gmail.com>
2017-12-13 23:21:37 +0100

    initial commit for msg app, readme updated

---
+++ full-stack-msg-app/fullStackMsgApp/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './shared/material/material.module';
+
+import 'hammerjs';
+
+import { AppComponent } from './app.component';
+import { MessagesComponent } from './messages/messages.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MessagesComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
