e1cf4d653976a2eecc92d682e442927b4267fb6f
saibot777 <stefantrajkovic777@gmail.com>
2018-01-15 15:06:42 +0100

    added sidenav UI Responsevness

--- materialContacts/src/app/shared/components/sidenav/sidenav.component.ts
+++ materialContacts/src/app/shared/components/sidenav/sidenav.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, NgZone, OnInit} from '@angular/core';
+
+const SMALL_WIDTH_BREAKPOINT = 720;
 
 @Component({
   selector: 'app-sidenav',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidenav.component.scss']
 })
 export class SidenavComponent implements OnInit {
+  private mediaMatcher: MediaQueryList = matchMedia(`(max-width: ${SMALL_WIDTH_BREAKPOINT}px)`);
 
-  constructor() { }
+  constructor(zone: NgZone) {
+    this.mediaMatcher.addListener(x =>
+      zone.run(() => this.mediaMatcher = x));
+  }
 
   ngOnInit() {
+  }
+
+  isScreenSmall(): boolean {
+    return this.mediaMatcher.matches;
   }
 
 }
