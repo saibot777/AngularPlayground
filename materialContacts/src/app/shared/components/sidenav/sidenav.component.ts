f87b36ff24c558cfc7fbd0f02e00e9da1b3d8fcd
saibot777 <stefantrajkovic777@gmail.com>
2018-01-15 16:32:17 +0100

    fetching data...

--- materialContacts/src/app/shared/components/sidenav/sidenav.component.ts
+++ materialContacts/src/app/shared/components/sidenav/sidenav.component.ts
@@ -1,4 +1,7 @@
 import {Component, NgZone, OnInit} from '@angular/core';
+import {UserService} from "../../../core/services/user.service";
+import {Observable} from "rxjs/Observable";
+import {User} from "../../../core/models/user.model";
 
 const SMALL_WIDTH_BREAKPOINT = 720;
 
@@ -9,13 +12,20 @@ const SMALL_WIDTH_BREAKPOINT = 720;
 })
 export class SidenavComponent implements OnInit {
   private mediaMatcher: MediaQueryList = matchMedia(`(max-width: ${SMALL_WIDTH_BREAKPOINT}px)`);
+  users: Observable<User[]>;
 
-  constructor(zone: NgZone) {
+  constructor(zone: NgZone, private userService: UserService) {
     this.mediaMatcher.addListener(x =>
       zone.run(() => this.mediaMatcher = x));
   }
 
   ngOnInit() {
+    this.users = this.userService.users;
+    this.userService.loadAll();
+
+    this.users.subscribe(data => {
+      console.log(data);
+    });
   }
 
   isScreenSmall(): boolean {
