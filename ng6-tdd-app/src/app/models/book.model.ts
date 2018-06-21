214ced2065d89a95a45141edc533e36a8845679a
saibot777 <stefantrajkovic777@gmail.com>
2018-06-21 13:03:39 +0200

    book model methods update

--- ng6-tdd-app/src/app/models/book.model.ts
+++ ng6-tdd-app/src/app/models/book.model.ts
@@ -14,4 +14,45 @@ export class BookModel implements BookInterface {
     public price: number,
     public upvotes: number = 0
   ) { }
+
+  public static find(title: string) {
+    const books: Array<BookModel> = JSON.parse(localStorage.getItem('books') || '[]');
+    for (const book of books) {
+      if (book.title === title) {
+        return new BookModel(
+          book.image,
+          book.title,
+          book.description,
+          book.price,
+          book.upvotes
+        );
+      }
+
+      return null;
+    }
+  }
+
+  save() {
+    const books: Array<BookModel> = JSON.parse(localStorage.getItem('books') || '[]');
+    books.forEach((item, index) => {
+      if (item.title === this.title) {
+        books.splice(index, 1);
+      }
+    });
+    books.push(this);
+    localStorage.setItem('books', JSON.stringify(books));
+
+    return true;
+  }
+
+  destroy() {
+    const books: Array<BookModel> = JSON.parse(localStorage.getItem('books') || '[]');
+    books.forEach((item, index) => {
+      books.splice(index, 1);
+      localStorage.setItem('books', JSON.stringify(books));
+    });
+
+    return null;
+  }
+
 }
