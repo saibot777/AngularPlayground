f1701ef9e95da4010ad8e58db3a4c5b77c77ba11
saibot777 <stefantrajkovic777@gmail.com>
2018-06-21 12:34:02 +0200

    book component tested

--- ng6-tdd-app/src/app/components/book/book.component.spec.ts
+++ ng6-tdd-app/src/app/components/book/book.component.spec.ts
@@ -1,10 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import * as faker from 'faker';
 import { BookComponent } from './book.component';
+import { BookModel } from "../../models/book.model";
 
 describe('BookComponent', () => {
   let component: BookComponent;
   let fixture: ComponentFixture<BookComponent>;
+  let book: BookModel;
+  let nativeElement: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,10 +19,34 @@ describe('BookComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BookComponent);
     component = fixture.componentInstance;
+    book = new BookModel(
+      faker.image.image(),
+      faker.lorem.sentence(),
+      faker.lorem.paragraph(),
+      12344.55,
+      0,
+    );
+    component.book = book;
     fixture.detectChanges();
+    nativeElement = fixture.nativeElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show the book image', () => {
+    const image = nativeElement.querySelector('.book-image').getAttribute('src');
+    expect(image).toEqual(book.image);
+  });
+
+  it('should show the book title', () => {
+    const title = nativeElement.querySelector('.book-title').innerHTML;
+    expect(title).toEqual(book.title);
+  });
+
+  it('should show the book description', () => {
+    const price = nativeElement.querySelector('.book-price').innerHTML;
+    expect(price).toEqual('$12,344.55');
   });
 });
