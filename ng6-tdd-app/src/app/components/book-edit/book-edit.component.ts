d20736a6178341e2919b62d252d2fcabfed3c06b
saibot777 <stefantrajkovic777@gmail.com>
2018-06-24 16:46:40 +0200

    book0list ui update

--- ng6-tdd-app/src/app/components/book-edit/book-edit.component.ts
+++ ng6-tdd-app/src/app/components/book-edit/book-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {BookModel} from "../../models/book.model";
 
 @Component({
   selector: 'app-book-edit',
@@ -6,8 +9,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./book-edit.component.css']
 })
 export class BookEditComponent implements OnInit {
+  bookEditForm: FormGroup;
+  book: BookModel;
+  activeForm = 'reactive';
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    fb: FormBuilder
+  ) {
+    this.bookEditForm = fb.group({
+      title: ['', Validators.required],
+      image: ['', Validators.required],
+      description: ['', Validators.required],
+      price: ['', Validators.required]
+    });
+    route.params.subscribe(res => {
+      this.book = BookModel.find(res['title']);
+      if (this.book === null) {
+        this.book = new BookModel('', '', '', 0);
+      }
+    });
+  }
+
+  submitReactiveForm() {
+    const bookData = this.prepareSaveBook();
+    this.book = new BookModel(
+      bookData.image,
+      bookData.title,
+      bookData.description,
+      bookData.price,
+    );
+    this.book.save();
+  }
+
+  prepareSaveBook() {
+    const formModel = this.bookEditForm.value;
+    return formModel;
+  }
 
   ngOnInit() {
   }
