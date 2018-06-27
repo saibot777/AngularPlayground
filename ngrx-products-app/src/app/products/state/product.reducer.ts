b3dbf1d673e2fb48fb3a87c1e5dbe31884fe655e
saibot777 <stefantrajkovic777@gmail.com>
2018-06-27 22:55:28 +0200

    products selectors initiated

--- ngrx-products-app/src/app/products/state/product.reducer.ts
+++ ngrx-products-app/src/app/products/state/product.reducer.ts
@@ -1,5 +1,6 @@
 import {Product} from '../product';
 import * as fromRoot from '../../state/app.state';
+import {createFeatureSelector, createSelector} from '@ngrx/store';
 
 export interface State extends fromRoot.State {
   products: ProductState;
@@ -16,6 +17,13 @@ const initialState: ProductState = {
   currentProduct: null,
   products: []
 };
+
+const getProductsFeatureState = createFeatureSelector<ProductState>('products');
+
+export const getShowProductCode = createSelector(
+  getProductsFeatureState,
+  state => state.showProductCode
+);
 
 export function reducer(state = initialState, action): ProductState {
   switch (action.type) {
