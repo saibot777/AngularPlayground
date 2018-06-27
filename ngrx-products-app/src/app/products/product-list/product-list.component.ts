d48ffa91c6f4e8a9c2806bc4c3f77e3dfd56baff
saibot777 <stefantrajkovic777@gmail.com>
2018-06-27 22:49:41 +0200

    state update

--- ngrx-products-app/src/app/products/product-list/product-list.component.ts
+++ ngrx-products-app/src/app/products/product-list/product-list.component.ts
@@ -38,11 +38,8 @@ export class ProductListComponent implements OnInit, OnDestroy {
 
     // TODO: Unsubscribe
     this.store.pipe(select('products')).subscribe(
-      products => {
-        if (products) {
-          this.displayCode = products.showProductCode;
-        }
-      });
+      products => this.displayCode = products.showProductCode
+    );
   }
 
   ngOnDestroy(): void {
